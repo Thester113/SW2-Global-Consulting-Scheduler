a78751705e7dc6765da220478fac18fd6c670099
tomhester <tomhester113@gmail.com>
2020-12-04 11:06:11 -0700

    Models

--- src/Views/Main.java
+++ src/Views/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//Extend Application
 public class Main extends Application {
 
     @Override
